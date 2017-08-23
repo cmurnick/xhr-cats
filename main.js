b459b8908e75a2f48c4a2d28e6785d1e7150a7ce
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-08-22 21:12:13 -0500

    followup json

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var CatCoralDiv = document.getElementById("kat-koral");
 
 
 
-function domString() {
+function domString(cats) {
 	var catString = "";
-	console.log(cats);
+	// console.log(cats);
 	for(var i = 0; i < cats.length; i++) {
 	
 
@@ -28,4 +28,50 @@ function writeToDom(strang) {
 
 
 
-domString(cats);
+// domString(cats);
+var cats = []
+function executeThisCodeAfterFileLoads (){
+	// console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	domString(data.cats);
+
+
+
+	// cats = dats.cats;
+	// myRequest2.send();
+}
+
+function executeThisCodeAfterFileLoads2 (){
+	// console.log("this", this.responseText);
+	var data = JSON.parse(this.responseText);
+	domString(data.dogs);
+
+
+	// domString(cats);
+}
+
+function executeThisCodeIfFileErrors (){
+	console.log("Shit's broke");
+
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "cats.json");
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest2.open("GET", "dogs.json");
+myRequest2.send();
+
+
+
+
+
+
+
+
+
